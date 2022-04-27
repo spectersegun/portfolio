73858eaa23163ae07c0064ab3cd248a74ce2b40a
spectersegun <segunomojolowo@gmail.com>
2022-04-27 13:22:45 +0100

    removed

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,9 +2,6 @@
 import React from 'react';
 import { useMediaQuery } from '@chakra-ui/media-query';
 import { Box, Flex, Heading, Text } from '@chakra-ui/layout';
-import Icon from '@chakra-ui/icon';
-import { DiCodeigniter, DiAndroid, DiWebplatform } from 'react-icons/di';
-// import specter from '/image/specter.jpg';
 
 function Profile() {
   const [isNotSmallerScreen] = useMediaQuery('(min-width:600px)');
